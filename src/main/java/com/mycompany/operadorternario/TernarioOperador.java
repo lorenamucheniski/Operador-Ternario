fed46ddf609249764cd9a4c49f88dd0410619ad2
lorenamucheniski <lorenamucheniski26@gmail.com>
2021-03-22 20:42:19 -0300

    first commit

---
+++ src/main/java/com/mycompany/operadorternario/TernarioOperador.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.operadorternario;
+
+/**
+ *
+ * @author lorena
+ */
+public class TernarioOperador {
+    
+    public static void main(String [] args) {
+        
+        int n1, n2, r;
+        n1 = 14;
+        n2 = 18;
+        r = (n1>n2)?n1+n2:n1-n2;
+        System.out.println(r);
+        
+    }
+}
